ec4fb65bd47f3b5031f61992cda1d675641cd926
JazmyneB <bradleyj1@email.arizona.edu>
2025-10-28 17:53:43 -0700

    feat(button): primary button

---
+++ jazzie-ui/src/components/PrimaryButton/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = ({
+    onClick,
+    disabled = false,
+    text = "Primary Button", //text to display on the button
+    buttonType = "primary" //type of button (primary, secondary, etc.)
+}) => {
+    
+    return (
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            className="primaryButton"
+        > {text}
+        </button>
+    )
+};
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool,
+    text: PropTypes.string,
+    
+};
+
+
+export default Button;
