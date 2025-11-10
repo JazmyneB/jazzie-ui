d3ffb03761031127b9df72f3e1f0c4e6bb04a754
JazmyneB <bradleyj1@email.arizona.edu>
2025-11-10 01:52:51 -0700

    feat(toggle): toggle & tests

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
-import Avatar from './components/Avatar/Avatar'
+import SoftToggle from './components/SoftToggle/SoftToggle';
 
 function App() {
+  const [toggleOn, setToggleOn] = useState(false);
+  const [bigToggle, setBigToggle] = useState(true);
   return (
     <div className="app-container">
       <NavBar
@@ -20,39 +22,43 @@ function App() {
       />
 
       <Hero />
-      <div style = {{ marginTop: '5rem', marginBottom: '5rem'}}>
-        <Avatar
-  size="lg"
-  initials="JS"
-  status="online"
-  badgeCount={3}
-  variant="gradient"
-/>
-<br />
-
-<Avatar
-  size="lg"
-  src="https://i.pravatar.cc/100"
-  status="online"
-  badgeCount={2}
-  variant="glass"
-/>
-<Avatar
-  size="md"
-  src="https://i.pravatar.cc/100"
-  status="offline"
-  badgeCount={0}
-  variant="bordered"
-/>
-<Avatar
-  size="sm"
-  src="https://i.pravatar.cc/100"
-  status="away"
-  badgeCount={5}
-  variant="gradient"
-/>
-
+      <div style={{ display: 'flex', gap: '2rem', flexDirection: 'column', padding: '2rem' }}>
+      
+      <div>
+        <h4>Default Toggle (md)</h4>
+        <SoftToggle
+          checked={toggleOn}
+          onChange={() => setToggleOn(!toggleOn)}
+        />
       </div>
+
+      <div>
+        <h4>Small Toggle (sm)</h4>
+        <SoftToggle
+          checked={toggleOn}
+          onChange={() => setToggleOn(!toggleOn)}
+          size="sm"
+        />
+      </div>
+
+      <div>
+        <h4>Large Toggle (lg)</h4>
+        <SoftToggle
+          checked={bigToggle}
+          onChange={() => setBigToggle(!bigToggle)}
+          size="lg"
+        />
+      </div>
+
+      <div>
+        <h4>Disabled Toggle</h4>
+        <SoftToggle
+          onChange={() => {}}
+          disabled
+        />
+      </div>
+
+    </div>
 
       <Footer />
     </div>
