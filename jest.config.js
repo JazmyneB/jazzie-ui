5620dcdd63e87401dfa98b4e2f4733eca3292def
JazmyneB <bradleyj1@email.arizona.edu>
2025-11-19 05:09:54 -0700

    test(buttonDocs): updated and passing

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,17 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!react-router-dom)/"
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
 };
