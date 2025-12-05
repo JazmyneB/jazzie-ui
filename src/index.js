8269ee6ef6532999660e08541c40ccd8b165fdea
JazmyneB <bradleyj1@email.arizona.edu>
2025-12-04 22:35:05 -0700

    feat(carousel): new component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
